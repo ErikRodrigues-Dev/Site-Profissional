9b4e21b94f4195f30c413a5c611c13548a8d84fe
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2022-08-29 16:33:11 -0300

    95% Finalizado

--- js/main.js
+++ js/main.js
@@ -29,3 +29,9 @@ var typed = new Typed(".typing", {
     backSpeed:60,
     loop:true
 });
+
+
+var btn = document.querySelector("#topo");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
